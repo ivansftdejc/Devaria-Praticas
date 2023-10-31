ec81844d84487aa84517605221f5bb46cc61753c
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-10-31 16:17:12 +0100

    Segregação dos arquivos JS

---
+++ Introdução-JS/js/arquivo_JS.js
@@ -0,0 +1,4 @@
+function M_Btn_Clicar_no_Btn()
+{
+    alert ('Você clicou no Botão')
+}
---
+++ Prática-Mini Site Devaria/public/js/_enviar_Comentário().js
@@ -0,0 +1,4 @@
+function M_enviar_Comentário()
+        {
+            alert('A sua mensagem foi enviada com sucesso');
+        }
