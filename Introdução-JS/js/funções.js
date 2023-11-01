799d3036709a8f1788e7951d99c272f403591b0f
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-01 11:20:28 +0100

    Funções

---
+++ Introdução-JS/js/funções.js
@@ -0,0 +1,14 @@
+function Mensagem(texto)
+{
+    alert(texto);
+}
+console.log(Mensagem(`Olá Mundo!`));
+
+Apresentar_mensagem=(texto)=>{
+    console.log(texto, "(*_*)");
+}
+
+Apresentar_mensagem('Mensagem1');
+Apresentar_mensagem('Mensagem3')
+Apresentar_mensagem('Mensagem4');
+Apresentar_mensagem('Mensagem5');
