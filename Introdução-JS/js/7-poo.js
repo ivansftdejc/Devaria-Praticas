411318c0538a0210a0d82c5276a9e9332b8c03c1
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-02 15:33:18 +0100

    Criação da class pessoa

---
+++ Introdução-JS/js/7-poo.js
@@ -0,0 +1,10 @@
+class Pessoa{
+    constructor(nome, idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+}
+let Pessoa1= new Pessoa();
+Pessoa1.nome='Ivandro'
+Pessoa1.idade=15
+console.log(Pessoa1.nome,Pessoa1.idade);
