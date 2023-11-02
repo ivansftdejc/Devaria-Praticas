ebba0b414df8969dca0f9b61ac759bc66d289ce5
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-02 16:26:46 +0100

    Implementação de uma herança

--- Introdução-JS/js/7-poo.js
+++ Introdução-JS/js/7-poo.js
@@ -10,7 +10,7 @@ Pessoa1.nome='João';
 Pessoa1.idade=15;
 console.log(Pessoa1.nome,Pessoa1.idade);
 
-//Criação da Classe Usuario sua validação
+//Criação da Classe Usuario e sua validação
 class Usuario{
     constructor(id,senha){
         this.Id=id;
@@ -18,8 +18,18 @@ class Usuario{
     }
     validação()
     {
-        return this.Id.includes('@') && this.Id.includes('.com') ?'E-mail Válido':'E-mail Inválido';
+        return this.Id.includes('@') && this.Id.includes('.com') ?'Válido':'Inválido';
     }
 }
 let Usuario1=new Usuario('ivandro@gmail.com','jhdjd');
 console.log(Usuario1.Id,Usuario1.Senha,Usuario1.validação());
+
+//Implementação de uma herança
+class Funcionario extends Usuario{
+    constructor(id,senha,nivel_acess){
+        super(id,senha);
+        this.Nivel_acess=nivel_acess;
+    }
+}
+const Funcionario1 = new Funcionario('ivanftdejc@gmail.com','kjdfh','Nivel 1');
+console.log(`O Id do funcionário é ${Funcionario1.Id} e é ${Funcionario1.validação()}`);
