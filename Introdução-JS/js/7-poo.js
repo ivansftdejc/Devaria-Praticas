605c79e091ae9b95df16e33fe50e9da84bb8ce26
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-02 16:10:54 +0100

    Criação da Classe Usuario sua validação

--- Introdução-JS/js/7-poo.js
+++ Introdução-JS/js/7-poo.js
@@ -1,3 +1,4 @@
+//Criação da Classe Pessoa
 class Pessoa{
     constructor(nome, idade){
         this.nome=nome;
@@ -5,6 +6,20 @@ class Pessoa{
     }
 }
 let Pessoa1= new Pessoa();
-Pessoa1.nome='Ivandro'
-Pessoa1.idade=15
+Pessoa1.nome='João';
+Pessoa1.idade=15;
 console.log(Pessoa1.nome,Pessoa1.idade);
+
+//Criação da Classe Usuario sua validação
+class Usuario{
+    constructor(id,senha){
+        this.Id=id;
+        this.Senha=senha;
+    }
+    validação()
+    {
+        return this.Id.includes('@') && this.Id.includes('.com') ?'E-mail Válido':'E-mail Inválido';
+    }
+}
+let Usuario1=new Usuario('ivandro@gmail.com','jhdjd');
+console.log(Usuario1.Id,Usuario1.Senha,Usuario1.validação());
