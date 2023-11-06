8bd367cdcb11ea2c78d58cd5506e618430d989ba
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-06 16:33:58 +0100

    Criação do exemplo pelo gefElementById

--- Introdução-JS/js/8-Manipulação_DOM.js
+++ Introdução-JS/js/8-Manipulação_DOM.js
@@ -1,8 +1,9 @@
 //Manipulção por Id: getElementById
-
+const Elemento_Id= document.getElementById('Manipulação_Id'); 
+console.log(Elemento_Id);
 //Manipulação por Classe: getElementByClassName
 
-//Manipulação por Tag: getElementByTag
-let Elementos_Tag= document.getElementsByTagName("div");
-const Elemento_Tag=Elementos_Tag[2]
+//Manipulação por Tag: getElementByTagName
+let Elementos_Tag= document.getElementsByTagName("h2");
+let Elemento_Tag=Elementos_Tag[1];
 console.log(Elemento_Tag);
