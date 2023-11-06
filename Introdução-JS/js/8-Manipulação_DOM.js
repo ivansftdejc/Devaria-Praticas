eed8be942c091b9b22dc4175b05920db952ded2c
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-06 16:06:37 +0100

    criação da manipulação por tag

--- Introdução-JS/js/8-Manipulação_DOM.js
+++ Introdução-JS/js/8-Manipulação_DOM.js
@@ -1,3 +1,8 @@
-//Manipulão por Id: getElementById
+//Manipulção por Id: getElementById
+
 //Manipulação por Classe: getElementByClassName
+
 //Manipulação por Tag: getElementByTag
+let Elementos_Tag= document.getElementsByTagName("div");
+const Elemento_Tag=Elementos_Tag[2]
+console.log(Elemento_Tag);
