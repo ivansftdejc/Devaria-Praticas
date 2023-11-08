09b4617d24bc9ad116de4fe9c2704ec9b597a2b0
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-08 16:55:51 +0100

    Implementação da margem superior em 4px

--- Introdução-JS/js/8-Manipulação_DOM.js
+++ Introdução-JS/js/8-Manipulação_DOM.js
@@ -13,9 +13,14 @@ console.log(Elemento_Tag);
 //Criação da função Alteração do HTML
 const Título= document.getElementById('título');
 const Subtítulo=document.getElementById('subtítulo');
+const Elemento_Margin_2=document.getElementsByClassName('mt-2');
 function alterarHtml(){
     Título.textContent="Título 1";
     Título.style.fontFamily="Segoe";
-    Título.style.fontSize="100px";
+    Título.style.fontSize="50px";
+    //Implementação da margem superior 4px
+    for(let elemento_margin of Elemento_Margin_2){
+        elemento_margin.className='mt-4 p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3'
+    }
 }
 
