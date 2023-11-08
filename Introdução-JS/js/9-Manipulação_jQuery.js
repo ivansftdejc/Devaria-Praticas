54254508caff3e75eac54941ceb1f5af4eab5620
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-08 19:44:28 +0100

    Implementação das modificação com jQuery

--- Introdução-JS/js/9-Manipulação_jQuery.js
+++ Introdução-JS/js/9-Manipulação_jQuery.js
@@ -1,35 +1,35 @@
-//Manipulção por Id: getElementById
-const Elemento_Id= document.getElementById('Manipulação_Id'); 
+//Manipulção por Id com jQuery: getElementById
+const Elemento_Id= $('#Manipulação_Id'); 
 console.log(Elemento_Id);
-//Manipulação por Classe: getElementByClassName
-const Elemento_Class=document.getElementsByClassName('mt-2');
+//Manipulação por Classe com jQuery: getElementByClassName
+const Elemento_Class=$('.mt-2');
 console.log(Elemento_Class)
 
-//Manipulação por Tag: getElementByTagName
-let Elementos_Tag= document.getElementsByTagName("h2");
+//Manipulação por Tag com jQuery: getElementByTagName
+let Elementos_Tag= $("h2");
 let Elemento_Tag=Elementos_Tag[1];
 console.log(Elemento_Tag);
 
-//Criação da função Alteração do HTML
-const Título= document.getElementById('título');
-const Texto_Html=document.getElementById('Texto');
-const Texto_input=document.getElementById('input_dinámico');
-const Elemento_Margin_2=document.getElementsByClassName('mt-2');
+//Criação da função Alteração do HTML com jQuery
+const Título= $('#título');
+const Texto_Html=$('#Texto');
+const Texto_input=$('#input_dinámico');
+const Elemento_Margin_2=$('.mt-2');
 function alterarHtml(){
-    Título.textContent=Texto_input.value;
-    Título.style.fontFamily="Segoe";
-    Título.style.fontSize="50px";
+    Título.text(Texto_input.val());
+    Título.css('fontFamily','Segoe');
+    Título.css('fontSize','34px');
     //Implementação da margem superior 4px
     for(let elemento_margin of Elemento_Margin_2){
-        elemento_margin.className='mt-4 p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3'
+        $(elemento_margin).addClass('mt-5 p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3');
     }
   
 
-    //Adicionando textos com DOM
+    //Adicionando textos com DOM e jQuery
     const novo_Parágrafo= document.createElement('p');
-    novo_Parágrafo.textContent=Texto_input.value;
-    Texto_Html.parentNode.appendChild(novo_Parágrafo);
+    $(novo_Parágrafo).text(Texto_input.val());
+    Texto_Html.after(novo_Parágrafo);
 
-    Texto_input.value='';
+    Texto_input.val('');
 }
 
