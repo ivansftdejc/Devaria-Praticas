499ccbfc2a3ac68ed9ceda99beba99172789e5f7
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-09 17:18:53 +0100

    Implementando API que gera fotos de gatos no HTML

--- Introdução-JS/js/10-API.js
+++ Introdução-JS/js/10-API.js
@@ -1,2 +1,15 @@
 
+const Div_Gatos=$('#gatos');
+function obterGato(){
+    fetch('https://cataas.com/cat')
+    .then(resultado=>resultado.blob())
+    .then(blob=>{
+        const Url_objecto_Image=URL.createObjectURL(blob);
+        const imagem=document.createElement('img');
+        imagem.src=Url_objecto_Image;
+        Div_Gatos.append(imagem);
+    
+    })
 
+    .catch(Erro => console.log('Erro ao executar API',Erro));
+}
