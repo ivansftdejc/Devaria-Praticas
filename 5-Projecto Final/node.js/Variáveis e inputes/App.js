6127f7a0d491bcf1c0f3a2f130c57786a6e67a34
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-22 12:03:00 +0100

    Implementação leitura da valores apartir do console com a fun

--- 5-Projecto Final/node.js/Variáveis e inputes/App.js
+++ 5-Projecto Final/node.js/Variáveis e inputes/App.js
@@ -1,2 +1,12 @@
-const minhaPrimeiraConstanteString='Minha primeira constante';
+//importando o Readline
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+const minhaPrimeiraConstanteString = 'Minha primeira constante';
 console.log(minhaPrimeiraConstanteString);
+let leituraDeCampo;
+readLine.question('Insira o valor que deseja ler ', input => {
+    leituraDeCampo = input;
+    console.log(`O valor que você leu é ${leituraDeCampo}`);  
+});
