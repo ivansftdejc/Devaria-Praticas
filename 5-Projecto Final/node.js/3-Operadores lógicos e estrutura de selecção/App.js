05f41c52364e1118b98b2be19e4e379d0c8b2e43
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-22 13:15:53 +0100

    Implementação da estrutura switch

--- 5-Projecto Final/node.js/3-Operadores lógicos e estrutura de selecção/App.js
+++ 5-Projecto Final/node.js/3-Operadores lógicos e estrutura de selecção/App.js
@@ -1,14 +1,30 @@
-const readLine=require('readline').createInterface({
+const readLine = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 console.log('Este Programa verifica se você tem idade para Conduzir')
 let idade;
-readLine.question('Quantos anos você tem?\n',input=>{
-    idade=input
-    if (idade>=18){
+readLine.question('Quantos anos você tem?\n', input => {
+    idade = input
+    if (idade >= 18) {
         console.log(`Parabéns!\n Você tem ${idade} anos e pode conduzir`);
-    }else{
+    } else {
         console.log('Você não pode Conduzir!');
     }
+    readLine.question('Qual é o seu nome?\n', nome => {
+        switch (nome) {
+            case 'Ivandro':
+                console.log('O seu nome foi encontrado no sistema');
+                break;
+            case 'David':
+                console.log('O seu nome foi encontrado no sistema');
+                break;
+            case 'Julieta':
+                console.log('O seu nome foi encontrado no sistema');
+                break;
+            default:
+                console.log('Nome Desconhecido');
+                break;
+        }
+    });
 })
