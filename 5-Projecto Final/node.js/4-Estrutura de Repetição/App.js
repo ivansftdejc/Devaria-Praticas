91573632aa176f4cfd1ffd1527236129382ea200
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-22 14:46:38 +0100

    Implementação da estrutura for of

--- 5-Projecto Final/node.js/4-Estrutura de Repetição/App.js
+++ 5-Projecto Final/node.js/4-Estrutura de Repetição/App.js
@@ -1,13 +1,13 @@
 const argumentos = process.argv.slice(2);
 //Estrutura for
 console.log('--------------Estrutura for---------------');
-for (let index = 0; index<argumentos.length; index++) {
+for (let index = 0; index < argumentos.length; index++) {
     console.log(`O argumento ${argumentos[index]} Pertence ao Índice ${index} `);
 }
 //Estrutura while
 console.log('--------------Estrutura While---------------');
 let index = 0;
-while (index<argumentos.length) {
+while (index < argumentos.length) {
     console.log(`O argumento ${argumentos[index]} Pertence ao Índice ${index} `);
     index++;
 }
@@ -17,9 +17,9 @@ index = 0;
 do {
     console.log(`O argumento ${argumentos[index]} Pertence ao Índice ${index} `);
     index++;
-}while (index<=argumentos.length)
+} while (index <= argumentos.length)
 //Estrutura For of
 console.log('--------------Estrutura For of---------------');
 for (const argumento of argumentos) {
-    console.log(argumento);   
+    console.log(argumento);
 }
